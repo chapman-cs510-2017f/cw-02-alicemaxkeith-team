1260665b0ed38bf2bcf3a5b476e4b7dff811f80c
solodovs <31462041+solodovs@users.noreply.github.com>
2017-09-12 19:12:37 -0700

    Update createfiles.sh

--- createfiles.sh
+++ createfiles.sh
@@ -1,3 +1,12 @@
-#!/bin/bash  
-
-echo ""
+!#/bin/bash/
+ 
+mkdir tmpfiles
+ 
+cd creatfiles
+ 
+for file in {001..100}
+   do
+   touch "tmpfiles/file$file.tmp"
+   echo "temporary $file" > tmpfiles/file$file.tmp
+ 
+done
