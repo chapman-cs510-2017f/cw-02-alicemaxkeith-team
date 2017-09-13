fced187c4ec45bbd5dd5ee1589e57359f941ac8c
alicewong1 <sowong@chapman.edu>
2017-09-13 04:45:40 +0000

    working fibs.sh

--- fibs.sh
+++ fibs.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-n=6
+
+n=$1
 inttest=${n%.*}
-
+ 
 first=1
-
+second=1
+ 
 echo "Number is $n"
 if [ $n -eq $inttest ]; then
-    echo "Number is integer"
-    
-    
-    for i in $(seq $n)
-        do
-            echo $first
-            temp = $(($first+$i))
-        done
-     
-
-
+        if [ $n -eq 1 ]; then
+                echo $first
+                exit 0
+        else
+                echo -n $first $second ""
+                for i in $(seq $(($n-2)))
+                 
+                 do
+                        temp=$(($first+$second))
+                        echo -n $temp ""
+                        first=$second
+                        second=$temp
+                done
+       fi
+        exit 0
 else
     echo "Number is not integer"
     exit 1
