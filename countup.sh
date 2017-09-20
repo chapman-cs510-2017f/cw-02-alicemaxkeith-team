b4ac77308c41504cec07123d9e6852b08ff74f9d
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-19 17:06:49 -0700

    Instructor comments

--- countup.sh
+++ countup.sh
@@ -3,9 +3,9 @@
  if [ $1 -gt 0 ];
  then
 	for i in $(seq $1); do echo -n "$i"; done;
-		echo
+		echo                                   ### The for loop is done. This should not be indented more than for
 		exit 0
-	else
+	else                                           ### else should have the same indentation as the if
 	  echo "Not a Positive Integer"
 	  exit 1
 	fi 
